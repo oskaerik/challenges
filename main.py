b55cb4860ed45802813f3696dbdd3e1c6525fb29
Oskar Eriksson <oskaerik@gmail.com>
2016-12-10 18:47:22 +0100

    Added basic file structure

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Main function for the sudoku solver"""
+from solver import Solver
+
+
+def main():
+    """Main function"""
+    print("Hello Sudoku World!")
+
+if __name__ == "__main__":
+    main();
---
+++ solver.py
@@ -0,0 +1,7 @@
+"""Contains the Solver class"""
+
+
+class Solver(object):
+    """The Solver class handles solving of the sudoku puzzle"""
+    def __init__(self):
+        print("I was created!")
